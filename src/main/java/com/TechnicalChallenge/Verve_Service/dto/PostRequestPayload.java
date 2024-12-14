95a890d128c3f28fb0a44f0a12fe9b35bb0e7b1e
Chetas Barhate <23421818+chetas24@users.noreply.github.com>
2024-12-14 13:48:49 +0530

    Updated

---
+++ src/main/java/com/TechnicalChallenge/Verve_Service/dto/PostRequestPayload.java
@@ -0,0 +1,38 @@
+package com.TechnicalChallenge.Verve_Service.dto;
+
+public class PostRequestPayload {
+
+    private Long id;
+    private String endpoint;
+    private int uniqueRequestCount;
+
+    public PostRequestPayload(Long id, String endpoint, int uniqueRequestCount) {
+        this.id = id;
+        this.endpoint = endpoint;
+        this.uniqueRequestCount = uniqueRequestCount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
+
+    public int getUniqueRequestCount() {
+        return uniqueRequestCount;
+    }
+
+    public void setUniqueRequestCount(int uniqueRequestCount) {
+        this.uniqueRequestCount = uniqueRequestCount;
+    }
+}
